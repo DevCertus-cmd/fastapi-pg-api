fd0e66438cd57bdc3196162120f646a2a2088c1e
DevCertus-cmd <support@certus.finance>
2025-04-23 15:17:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 
 @app.get("/get-data")
 def get_data(merchant: str = Query(...), date: str = Query(...)):
+    start_datetime = f"{date} 00:00:00"
+    end_datetime = f"{date} 23:59:59"
+
     conn = psycopg2.connect(
         host=os.environ["DB_HOST"],
         dbname=os.environ["DB_NAME"],
@@ -15,8 +18,14 @@ def get_data(merchant: str = Query(...), date: str = Query(...)):
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT col1, col2 FROM stats WHERE merchant = %s AND date = %s",
-        (merchant, date)
+        """
+        SELECT col1, col2
+        FROM stats
+        WHERE merchant_name = %s
+          AND create_time >= %s
+          AND create_time <= %s
+        """,
+        (merchant, start_datetime, end_datetime)
     )
     row = cur.fetchone()
     cur.close()
@@ -26,3 +35,4 @@ def get_data(merchant: str = Query(...), date: str = Query(...)):
         return {"col1": row[0], "col2": row[1]}
     else:
         return {"col1": None, "col2": None}
+
